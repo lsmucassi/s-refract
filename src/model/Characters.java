bd457e2d098c9e786e19561ab9c0d8ffa235632c
Linda MUCASSI <lindasmucassi@gmail.com>
2018-08-30 13:53:17 +0200

    Do: save players

--- src/model/Characters.java
+++ src/model/Characters.java
@@ -3,6 +3,8 @@ package model;
 import ErrException.EIOException;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Characters {
@@ -16,7 +18,7 @@ public class Characters {
     int def = 0;
     String weap1 = "";
     String weap2 = "";
-//    String[] collWeap;
+    String[][] fileData;
     String name = "";
     String type = "";
     String clan = "";
@@ -68,9 +70,10 @@ public class Characters {
         this.def = df;
     }
 
-
-
     //get
+    public String getName() {
+        return this.name;
+    }
     public int getLife() {
         return this.life;
     }
@@ -87,90 +90,4 @@ public class Characters {
         return this.exp;
     }
 
-    //get players
-    public void loadPlayer() {
-        String fileName = "src/players.txt";
-        File file = new File(fileName);
-
-        try {
-            Scanner inFile = new Scanner(file);
-            while (inFile.hasNext()) {
-                String line = inFile.next();
-                //System.out.println( line + " ->");
-
-            }
-            System.out.println("\n");
-            inFile.close();
-
-        } catch (FileNotFoundException err) {
-            System.out.println("Err: Players file not found [ error loading heroes ]");
-            err.printStackTrace();
-        }
-
-        Scanner creatCha = new Scanner(System.in);
-        int doneCreat = 0;
-
-        System.out.println("\t \033[34m - C : Would you like to create a hero \033[0m");
-        System.out.println("\t \033[31m - S : Choose from a list \033[0m");
-        System.out.println("------------------------------------------------------------------------------------");
-        System.out.println("\033[32m $ Enter command \033[0m");
-        System.out.print("\033[32m $  \033[0m");
-
-        String nextCha = creatCha.nextLine();
-        if (nextCha == "C") {
-            while (doneCreat == 0) {
-                //enter Hero characteristics
-            }
-        } else {
-        }
-
-//
-//        try {
-//            String fileName = "/goinfre/lmucassi/Documents/s-refract/src/players.txtplayers.txt";
-//            FileInputStream fstream = new FileInputStream(fileName);
-//            BufferedReader breader = new BufferedReader(new InputStreamReader(fstream));
-//            String fLine;
-//            int line = 1;
-//            String[] splits;
-//
-//            while ((fLine = breader.readLine()) != null) {
-//                if (line == 1)
-//                    try {
-//                        iter = Integer.parseInt(fLine);
-//                        if (iter < 0) {
-//                            System.out.println("Error: line must be an integer and must be positive");
-//                            return;
-//                        }
-//                    } catch (NumberFormatException e) {
-//                        System.out.println("Error: expected a positive integer");
-//                        return;
-//                    }
-//                else {
-//                    splits = fLine.split(" ");
-//
-//                    if (splits.length == 1 && splits[0].isEmpty()) continue;
-//                    if (splits.length != 5)
-//                        throw new Exception("Error: ["+ line +"] - must have % parameters only");
-//
-//                    try {
-////                    aCraft.newAircraft(splits[0], splits[1], Integer.parseInt(splits[2]),
-////                            Integer.parseInt(splits[3]),
-////                            Integer.parseInt(splits[4])).registerTower(opTower);
-//                    } catch (NumberFormatException e) {
-//                        System.out.println("Error: ["+ line +"] - parameters 3 to 5 must be positive integers");
-//                        return ;
-//                    }
-//                }
-//                line++;
-//            }
-//            breader.close();
-//        }
-//        catch (EIOException e) {
-//            System.out.println("Error: "+ e.getMessage());
-//            return ;
-//        } catch (Exception e) {
-//            System.out.println("Error: " + e.getMessage());
-//            return ;
-//        }
-    }
 }
--- src/model/Heroes.java
+++ src/model/Heroes.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Heroes extends Characters{
 
     int X, Y;
@@ -18,6 +23,121 @@ public class Heroes extends Characters{
 
     public int getY() {
         return Y;
+    }
+
+
+    private Heroes getHero(String details) {
+        Heroes hero = new Heroes();
+        String[] detail = details.split(",");
+
+        hero.setName(detail[0]);
+
+        return (hero);
+    }
+
+    //list players
+    public  void listPlayers(Scanner ls) {
+        ArrayList<Heroes> players = new ArrayList<>();
+
+         while (ls.hasNext()) {
+            players.add(getHero(ls.next()));
+        }
+        hero = players.get(3);
+        for (Heroes h:
+             players) {
+            System.out.println(h.getName());
+        }
+    }
+
+    //get players
+    public void loadPlayer() {
+        String fileName = "src/players.txt";
+        File file = new File(fileName);
+
+        try {
+            Scanner inFile = new Scanner(file);
+
+            listPlayers(inFile);
+//
+//            while (inFile.hasNext()) {
+//                String line = inFile.next();
+//                  //System.out.println( line + " ->");
+//
+//            }
+
+            System.out.println("\n");
+            inFile.close();
+
+        } catch (FileNotFoundException err) {
+            System.out.println("Err: Players file not found [ error loading heroes ]");
+            err.printStackTrace();
+        }
+
+        Scanner creatCha = new Scanner(System.in);
+        int doneCreat = 0;
+
+        System.out.println("\t \033[34m - C : Would you like to create a hero \033[0m");
+        System.out.println("\t \033[31m - S : Choose from a list \033[0m");
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("\033[32m $ Enter command \033[0m");
+        System.out.print("\033[32m $  \033[0m");
+
+        String nextCha = creatCha.nextLine();
+        if (nextCha == "C") {
+            while (doneCreat == 0) {
+                //enter Hero characteristics
+            }
+        } else {
+        }
+
+//
+//        try {
+//            String fileName = "/goinfre/lmucassi/Documents/s-refract/src/players.txtplayers.txt";
+//            FileInputStream fstream = new FileInputStream(fileName);
+//            BufferedReader breader = new BufferedReader(new InputStreamReader(fstream));
+//            String fLine;
+//            int line = 1;
+//            String[] splits;
+//
+//            while ((fLine = breader.readLine()) != null) {
+//                if (line == 1)
+//                    try {
+//                        iter = Integer.parseInt(fLine);
+//                        if (iter < 0) {
+//                            System.out.println("Error: line must be an integer and must be positive");
+//                            return;
+//                        }
+//                    } catch (NumberFormatException e) {
+//                        System.out.println("Error: expected a positive integer");
+//                        return;
+//                    }
+//                else {
+//                    splits = fLine.split(" ");
+//
+//                    if (splits.length == 1 && splits[0].isEmpty()) continue;
+//                    if (splits.length != 5)
+//                        throw new Exception("Error: ["+ line +"] - must have % parameters only");
+//
+//                    try {
+////                    aCraft.newAircraft(splits[0], splits[1], Integer.parseInt(splits[2]),
+////                            Integer.parseInt(splits[3]),
+////                            Integer.parseInt(splits[4])).registerTower(opTower);
+//                    } catch (NumberFormatException e) {
+//                        System.out.println("Error: ["+ line +"] - parameters 3 to 5 must be positive integers");
+//                        return ;
+//                    }
+//                }
+//                line++;
+//            }
+//            breader.close();
+//        }
+//        catch (EIOException e) {
+//            System.out.println("Error: "+ e.getMessage());
+//            return ;
+//        } catch (Exception e) {
+//            System.out.println("Error: " + e.getMessage());
+//            return ;
+//        }
     }
 
 }
